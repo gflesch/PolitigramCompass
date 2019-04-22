b4fc99431beee8b4527d3167df7a8db9011b0e00
gflesch <44884663+gflesch@users.noreply.github.com>
2019-04-22 15:32:39 -0400

    Update code.js

--- code.js
+++ code.js
@@ -81,17 +81,17 @@ function endTest(){
 }
 function calculatePlacement(variable){
   if (variable === "x"){
-  xValue = (ans1 + ans2 + ans3 + ans4 + ans5 + ans6 + ans7 + ans8 + ans9 + ans10 + ans11 + ans12 - ans13 - ans14 - ans15 - ans16 - ans17 - ans18 - ans19 - ans20 - ans21 - ans22 - ans23 - ans24)/24;
+  xValue = (ans1 + ans2 + ans3 + ans4 + ans5 + ans6 + ans7 + ans8 + ans9 + ans10 + ans11 + ans12 - ans13 - ans14 - ans15 - ans16 - ans17 - ans18 - ans19 - ans20 - ans21 - ans22 - ans23 - ans24)/12;
   xPosition = xValue*16+155;
   xCoord = (xValue).toFixed(2);
   }
   if (variable === "y"){
-  yValue = (ans25 + ans26 + ans27 + ans28 + ans29 + ans30 + ans31 + ans32 + ans33 + ans34 - ans35 - ans36 - ans37 - ans38 - ans39 - ans40 - ans41 - ans42 - ans43 - ans44)/20;
+  yValue = (ans25 + ans26 + ans27 + ans28 + ans29 + ans30 + ans31 + ans32 + ans33 + ans34 - ans35 - ans36 - ans37 - ans38 - ans39 - ans40 - ans41 - ans42 - ans43 - ans44)/10;
   yPosition = yValue*16+195;
   yCoord = (-(yValue)).toFixed(2);
   }
   if (variable === "z"){
-  zValue = (ans45 + ans46 + ans47 + ans48 + ans49 + ans50 + ans51 + ans52 + ans53 + ans54 - ans55 - ans56 - ans57 - ans58 - ans59 - ans60 - ans61 - ans62 - ans63 - ans64)/20;
+  zValue = (ans45 + ans46 + ans47 + ans48 + ans49 + ans50 + ans51 + ans52 + ans53 + ans54 - ans55 - ans56 - ans57 - ans58 - ans59 - ans60 - ans61 - ans62 - ans63 - ans64)/10;
   zPosition = zValue*16+160;
   zCoord = (zValue).toFixed(2); 
   }
